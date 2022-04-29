601fc370ecfdde6f101e2bfbb106b915f99b2d9b
KW <kenneth_sl_wong@msn.com>
2022-04-29 18:53:29 -0400

    added service worker

--- public/js/idb.js
+++ public/js/idb.js
@@ -2,22 +2,22 @@ let db;
 const request = indexedDB.open('budget_tracker', 1);
 
 //Initializes the transaction object store on version change or first use
-request.onupgradeneeded = function(event) {
+request.onupgradeneeded = function (event) {
     const db = event.target.result;
 
-    db.createObjectStore('transactions', {autoIncrement: true});
+    db.createObjectStore('transactions', { autoIncrement: true });
 };
 
-request.onsuccess = function(event) {
+request.onsuccess = function (event) {
     db = event.target.result;
 
     //Check if online, if yes send to api
-    if(navigator.onLine) {
+    if (navigator.onLine) {
         uploadTransaction();
     }
 };
 
-request.onerror = function(event) {
+request.onerror = function (event) {
     console.log(event.target.errorCode);
 };
 
@@ -26,7 +26,8 @@ function saveRecord(record) {
 
     const transactionStore = transaction.objectStore('transactions');
 
-    transactionStore.add(record);
+    transactionStore.add(record)
+    alert('Transaction added!');
 };
 
 function uploadTransaction() {
@@ -37,32 +38,35 @@ function uploadTransaction() {
     const getAll = transactionStore.getAll();
 
     getAll.onsuccess = function () {
-        if(getAll.result.length > 0) {
+        if (getAll.result.length > 0) {
             fetch('/api/transaction', {
                 method: 'POST',
                 body: JSON.stringify(getAll.result),
                 headers: {
-                  Accept: 'application/json, text/plain, */*',
-                  'Content-Type': 'application/json'
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json'
                 }
-              })
-              .then(response => response.json())
-        .then(serverResponse => {
-          if (serverResponse.message) {
-            throw new Error(serverResponse);
-          }
+            })
+                .then(response => response.json())
+                .then(serverResponse => {
+                    if (serverResponse.message) {
+                        throw new Error(serverResponse);
+                    }
 
-          const transaction = db.transaction(['transactions'], 'readwrite');
+                    const transaction = db.transaction(['transactions'], 'readwrite');
 
-          const transactionStore = transaction.objectStore('transactions');
-          
-          transactionStore.clear();
+                    const transactionStore = transaction.objectStore('transactions');
 
-          alert('All cached transactions saved');
-        })
-        .catch(err => {
-          console.log(err);
-        });
+                    transactionStore.clear();
+
+                    alert('All cached transactions saved');
+                    populateTotal();
+                    populateTable();
+                    populateChart();
+                })
+                .catch(err => {
+                    console.log(err);
+                });
         }
     }
 };
